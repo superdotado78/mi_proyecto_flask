cda33450fce5829fb803a04e165305aba22ff791
Jorge Diaz mancheno <jl.diazm@uea.edu.ec>
2025-08-21 17:47:26 -0500

    sem10

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "¡Hola, Flask está funcionando!"
+    return render_template("index.html", titulo="Inicio", nombre=None)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", titulo="Acerca de")
 
 @app.route("/usuario/<nombre>")
 def usuario(nombre):
-    return f'Bienvenido a mi página, {nombre}!'
+    return render_template("index.html", titulo="Usuario", nombre=nombre)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
